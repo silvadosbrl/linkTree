a5f538dcf2c38c01c8fb688ac793c240edefc470
Ian Campos <142848165+silvadosbrl@users.noreply.github.com>
2025-03-25 06:08:19 -0300

    linkTree1.0

---
+++ js/js.js
@@ -0,0 +1,11 @@
+document.getElementById("redirect").addEventListener("click", function(){
+    window.location.href ="https://www.instagram.com/galegoreboque1/";
+});
+
+document.getElementById("redirectTwo").addEventListener("click", function(){
+    window.location.href ="https://wa.me/+5581992888582";
+});
+
+document.getElementById("services").addEventListener("click", function(){
+    window.location.href ="secondPage.html";
+});
